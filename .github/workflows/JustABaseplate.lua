ff88b196d321af9d5fe6b8de287e7390569999ca
sparezirt <sonixery@gmail.com>
2025-06-11 18:36:37 +0700

    Update JustABaseplate.lua

--- .github/workflows/JustABaseplate.lua
+++ .github/workflows/JustABaseplate.lua
@@ -3,5 +3,5 @@ task.wait(3)
 
 
 loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Just-A-Baseplate-Working-Reanimation-39126"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/krystalDanceV3Modded/refs/heads/patch-1/KrystalDance3.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/Script/refs/heads/main/.github/workflows/KDV3Modded.txt"))()
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Hi whoever saw this TestFeBypass is trying to skid off my script.")
