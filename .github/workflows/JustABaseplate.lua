9139c72b6c5e3e8d0162d606259b97bfeac11808
sparezirt <sonixery@gmail.com>
2025-06-11 18:34:56 +0700

    Create JustABaseplate.lua

---
+++ .github/workflows/JustABaseplate.lua
@@ -0,0 +1,7 @@
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Loading Krystal Dance V3 Modded..")
+task.wait(3)
+
+
+loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Just-A-Baseplate-Working-Reanimation-39126"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/krystalDanceV3Modded/refs/heads/patch-1/KrystalDance3.txt"))()
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Hi whoever saw this TestFeBypass is trying to skid off my script.")
