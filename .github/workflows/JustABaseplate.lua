92c22045f5fe4f6749ca61c96531a1277651486e
sparezirt <sonixery@gmail.com>
2025-06-14 09:02:46 +0700

    Update JustABaseplate.lua

--- .github/workflows/JustABaseplate.lua
+++ .github/workflows/JustABaseplate.lua
@@ -4,4 +4,4 @@ task.wait(3)
 
 loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Just-A-Baseplate-Working-Reanimation-39126"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/Script/refs/heads/main/.github/workflows/KDV3Modded.txt"))()
-game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Hi whoever saw this TestFeBypass is trying to skid off my script.")
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Loaded.")
